29425fdcf0f5f95a7c58f4a505f1b59f53dd97b4
5151002 <5151002@st.hsc.ac.jp>
2017-09-01 10:22:15 +0900

    画面関係調整

--- app/src/main/java/com/example/yuichi_oba/ecclesia/activity/ReserveConfirmActivity.java
+++ app/src/main/java/com/example/yuichi_oba/ecclesia/activity/ReserveConfirmActivity.java
@@ -206,7 +206,10 @@ public class ReserveConfirmActivity extends AppCompatActivity
                 break;
             // 「予約変更」が選択された
             case R.id.option_reserveChange:
-                Toast.makeText(this, "予約変更", Toast.LENGTH_SHORT).show();
+                intent = new Intent(getApplicationContect(), ReserveConfirmActivity.class);
+                intent.putExtra("Change", reserveInfo);
+                startActiviti(intent);
+//                Toast.makeText(this, "予約変更", Toast.LENGTH_SHORT).show();
                 // 予約情報インスタンスを次の画面にオブジェクト渡しする
                 break;
             // 「延長」が選択された
