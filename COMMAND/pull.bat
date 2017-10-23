5b909848bb60e494af67a9e6ca538a63a4138c0b
SOPHIA\5151021 <5151021@st.hsc.ac.jp>
2017-10-23 11:41:46 +0900

    ecclesia sql pull/push.bat update

--- COMMAND/pull.bat
+++ COMMAND/pull.bat
@@ -2,4 +2,4 @@
 # さきに、pull_command.txtを実行
 # pull.bat "自分がコピーを置きたいディレクトリ名" // (ex) /Users/5151021/Desktop
 echo "pull batch start!"
-adb pull /sdcard/sample.db %1
+adb pull /sdcard/ecclesia.db %1
--- COMMAND/push.bat
+++ COMMAND/push.bat
@@ -1,2 +1,2 @@
 # push.bat "DB–¼" (ex) push.bat Users/5151021/sample.db
-adb push %1 /sdcard/sample.db
+adb push %1 /sdcard/ecclesia.db
